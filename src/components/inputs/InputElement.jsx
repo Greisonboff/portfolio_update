20460dd11ee5889317766dc8ac9d1586240429c4
Greison Nicolau Boff <greison.boff.external@lojayoucom.com.br>
2023-10-12 21:38:07 -0300

    ajust logo

--- src/components/inputs/InputElement.jsx
+++ src/components/inputs/InputElement.jsx
@@ -8,7 +8,7 @@ export default function InputElement({ type, placeholder, aoAlterado, valor }) {
     return (
         <>
             <TituloInput texto={placeholder} />
-            <input className="outline-none p-2 rounded-md dark:bg-zinc-700 dark:placeholder-white dark:text-white border-solid border-2 bg-white dark:border-0 border-slate-950 placeholder-slate-600" type={type} onChange={aoDigitado} value={valor} required placeholder={placeholder}></input>
+            <input className="outline-none p-2 rounded-md dark:bg-zinc-700 dark:placeholder-slate-200 dark:text-white border-solid border-2 bg-white dark:border-0 border-slate-950 placeholder-slate-600" type={type} onChange={aoDigitado} value={valor} required placeholder={placeholder}></input>
         </>
     )
 }
