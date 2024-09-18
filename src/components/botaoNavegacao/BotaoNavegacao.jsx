9639e3148a9128ac8344d318378028de900332ce
Greison Nicolau Boff <greison.boff.external@lojayoucom.com.br>
2024-09-17 21:59:48 -0300

    feat: implementação dark mode no modal de edição

--- src/components/botaoNavegacao/BotaoNavegacao.jsx
+++ src/components/botaoNavegacao/BotaoNavegacao.jsx
@@ -4,7 +4,7 @@ function Botao_navegacao({ text, funcao }) {
   return (
     <button
       onClick={funcao}
-      className="dark:text-slate-300 text-gray-600 m-1 lg:m-4 rounded-md border-2 border-solid dark:border-slate-300 border-slate-600 p-2 dark:hover:text-gray-400 hover:border-transparent hover:animate-pulse"
+      className="dark:text-slate-300 text-gray-600 m-1 lg:m-4 rounded-md border-2 border-solid dark:border-slate-300 border-slate-600 p-2 dark:hover:text-gray-400 hover:border-[#000] hover:animate-pulse"
     >
       {text}
     </button>
